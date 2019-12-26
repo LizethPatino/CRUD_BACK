8d483c6739dc11ddb26853734757193cb65a535e
Lizeth Patiño <lizeth0330@outlook.com>
2019-12-26 14:03:15 -0500

    creación del proyecto y creación de servicios

---
+++ src/main/java/com/sinfloo/ejemplo01/Persona.java
@@ -0,0 +1,44 @@
+
+package com.sinfloo.ejemplo01;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "personas")
+public class Persona {
+    
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String nombre;
+    @Column 
+    private String apellidos;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+    
+}
